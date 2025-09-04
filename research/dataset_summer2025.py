bcdea832817fcd6456255028209fc99d78a5fc5e
Harry de Boer <hldeboer@gmail.com>
2025-09-04 13:05:33 +0200

    utc correction

--- research/dataset_summer2025.py
+++ research/dataset_summer2025.py
@@ -2,8 +2,8 @@ import meet_je_stad_api_service
 
 
 meet_je_stad_api_service.MeetJeStadAPIService().get_data(
-            '2025-06-01,00:00',
-            '2025-08-31,23:59',
+            '2025-05-31,22:00',
+            '2025-08-31,21:59',
     'sensors',
 'csv',
             'Utrecht',
